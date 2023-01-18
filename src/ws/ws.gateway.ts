c49c63a67b7017081b42eac02365415ddef7449b
Romulo Campos <56273783+romulocrj@users.noreply.github.com>
2023-01-18 00:46:52 -0300

    ajuste ws

--- src/ws/ws.gateway.ts
+++ src/ws/ws.gateway.ts
@@ -9,14 +9,21 @@ import { ConnectedSocket } from '@nestjs/websockets/decorators';
 import { Server, Socket } from 'socket.io';
 import { ConsultaDto, ProfissionalDto } from './dto/ws.dto';
 
-export const mapClientConsulta = new Map<string, ConsultaDto>();
-export const mapConsultaClient = new Map<number, Set<string>>();
 
+enum Events {
+  iniciar_consulta_event = 'iniciar_consulta',
+  finalizar_consulta_event = 'finalizar_consulta',
+  consultas_ativas_event = 'consultas',
+}
+enum ClientType {
+  MEDICO = 0,
+  PACIENTE = 1,
+}
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
-  path: "/ws"
+  path: '/ws',
   //transports: ['websocket'],
 })
 export class WsGateway implements OnGatewayDisconnect<Socket> {
@@ -28,7 +35,8 @@ export class WsGateway implements OnGatewayDisconnect<Socket> {
     @MessageBody() data: ProfissionalDto,
     @ConnectedSocket() client: Socket,
   ): Promise<any> {
-    (client as any).tipo = 'medico';
+    (client as any).tipo = ClientType.MEDICO;
+    (client as any).profissional = data.profissional;
     client.join(`room-p-${data.profissional}`);
     await this.internalSendConnections(data.profissional);
   }
@@ -38,31 +46,27 @@ export class WsGateway implements OnGatewayDisconnect<Socket> {
     @MessageBody() data: ConsultaDto,
     @ConnectedSocket() client: Socket,
   ): Promise<any> {
-    const set = (mapConsultaClient[data.consulta] ??
-      new Set<string>()) as Set<string>;
+    this.server
+      .to(`room-consulta-${data.consulta}`)
+      .emit(Events.iniciar_consulta_event, { consulta: data.consulta });
+
+      console.log('Medico: '+(client as any).profissional,Events.iniciar_consulta_event, { consulta: data.consulta })
 
     client.join(`room-consulta-${data.consulta}`);
-
-    console.log(set);
-    set.forEach((v) => {
-      this.server.to(v).emit('iniciar_consulta', { consulta: data.consulta });
-    });
   }
   @SubscribeMessage('consultaFinalizar')
   async consultaFinalizar(
     @MessageBody() data: ConsultaDto,
     @ConnectedSocket() client: Socket,
   ): Promise<any> {
-    
-    const set = (mapConsultaClient[data.consulta] ??
-      new Set<string>()) as Set<string>;
-
-    //console.log(set);
-    set.forEach((v) => {
-      this.server.to(v).emit('finalizar_consulta', { consulta: data.consulta });
-    });
-
+    // Médico fechando a consulta
     client.leave(`room-consulta-${data.consulta}`);
+
+    this.server
+      .to(`room-consulta-${data.consulta}`)
+      .emit(Events.finalizar_consulta_event, { consulta: data.consulta });
+
+      console.log('Medico: '+(client as any).profissional,Events.finalizar_consulta_event, { consulta: data.consulta })
   }
 
   async internalSendConnections(profissional: number) {
@@ -73,16 +77,21 @@ export class WsGateway implements OnGatewayDisconnect<Socket> {
       .fetchSockets();
 
     const consultas = new Set<number>();
+    const medicos = new Set<string>();
 
     sockets.forEach((s) => {
-      const consulta = mapClientConsulta[s.id];
-      if (consulta && !consultas.has(consulta))
-        consultas.add(consulta.consulta);
+      if ((s as any).tipo == ClientType.MEDICO) {
+        medicos.add(s.id);
+      } else {
+        if ((s as any).consulta && !consultas.has((s as any).consulta))
+          consultas.add((s as any).consulta);
+      }
     });
-
-    this.server
-      .to(`room-p-${profissional}`)
-      .emit('consultas', { consultas: Array.from(consultas) });
+    const _c= Array.from(consultas);
+    console.log('Medico: '+profissional,Events.consultas_ativas_event, _c)
+    this.server.to(Array.from(medicos)).emit(Events.consultas_ativas_event, {
+      consultas: _c,
+    });
   }
 
   @SubscribeMessage('consultaPacienteJoin')
@@ -90,21 +99,12 @@ export class WsGateway implements OnGatewayDisconnect<Socket> {
     @MessageBody() data: ConsultaDto,
     @ConnectedSocket() client: Socket,
   ): Promise<any> {
-    (client as any).tipo = 'paciente';
-    client.join(`room-p-${data.profissional}`);
-
-    //client.join(`room-consulta-${data.consulta}`);
-
+    (client as any).tipo = ClientType.PACIENTE;
     (client as any).consulta = data.consulta;
-    mapClientConsulta[client.id] = data;
-    const set = (mapConsultaClient[data.consulta] ??
-      new Set<string>()) as Set<string>;
-    if (!set.has(client.id)) set.add(client.id);
-    mapConsultaClient[data.consulta] = set;
-    console.log(mapClientConsulta);
-    /*this.server
-      .to(`room-p-${data.profissional}`)
-      .emit('connected', { consulta: data.consulta });*/
+    (client as any).profissional = data.profissional;
+
+    client.join(`room-p-${data.profissional}`);
+    client.join(`room-consulta-${data.consulta}`);
 
     await this.internalSendConnections(data.profissional);
 
@@ -112,31 +112,20 @@ export class WsGateway implements OnGatewayDisconnect<Socket> {
       .in(`room-consulta-${data.consulta}`)
       .fetchSockets();
 
-    if (sockets.length > 0) {
-      client.emit('iniciar_consutla', { consulta: data.consulta });
+    if (sockets.find((s: any) => s.tipo == ClientType.MEDICO)) {
+      client.emit(Events.iniciar_consulta_event, { consulta: data.consulta });
     }
 
     return data;
   }
 
   handleDisconnect(client: Socket) {
-    if ((client as any).tipo == 'medico') return;
-
-    const consulta = mapClientConsulta[client.id];
-    console.log('Disconnected ' + client.id, consulta);
-
-    if (consulta) {
-      delete mapClientConsulta[client.id];
-
-      //client.to(roomId).emit('disconnected', { consulta: consulta });
-      this.internalSendConnections(consulta.profissional);
-
-      const set = (mapConsultaClient[consulta] ??
-        new Set<string>()) as Set<string>;
-      if (set.has(client.id)) {
-        set.delete(client.id);
-        if (set.size == 0) delete mapConsultaClient[consulta];
-      }
+    if ((client as any).tipo == ClientType.MEDICO) {
+      console.log('Disconnected medico: ', (client as any).profissional);
+    } else {
+      console.log('Disconnected consulta: ', (client as any).consulta);
     }
+
+    this.internalSendConnections((client as any).profissional);
   }
 }
